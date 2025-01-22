26b356e5f45bb30e4dc076f7957b04885bc5deb8
Afdal-B <159702730+Afdal-B@users.noreply.github.com>
2025-01-22 22:07:29 +0100

    Create a function to compute the kernel

--- src/models/LeniaDiscrete.py
+++ src/models/LeniaDiscrete.py
@@ -7,27 +7,61 @@
   return @kernel, @kernel_FFT
 end'''
 import numpy as np
+import matplotlib.pyplot as plt
 class DiscreteLenia:
     def __init__(self,SIZE_X, SIZE_Y):
         self.SIZE_X = SIZE_X
         self.SIZE_Y = SIZE_Y
 
     def pre_calculate_kernel(self, beta, dx):
-       radius = self.get_polar_radius_matrix(self.SIZE_X,self.SIZE_Y)*dx
-       Br = beta.size[0] * radius
-       kernel_shell = beta[np.floor(Br)] * self.kernel_core(Br % 1)
+       radius = self.get_polar_radius_matrix()*dx
+       Br = beta.shape[0] * (radius / np.max(radius)) # Normalisation
+       kernel_shell = beta[np.floor(Br).astype(int)%beta.shape[0]] * self.kernel_core(Br % 1)
        kernel = kernel_shell /np.sum(kernel_shell)
-       kernel_FFT = np.fft.fft2(kernel)
-       return kernel, kernel_FFT
+       #kernel_FFT = np.fft.fft2(kernel)
+       return kernel_shell
     
-    def get_polar_radius_matrix(self, SIZE_X, SIZE_Y):
-        x = np.arange(-SIZE_X/2, SIZE_X/2)
-        y = np.arange(-SIZE_Y/2, SIZE_Y/2)
+    def get_polar_radius_matrix(self):
+        x = np.arange(-self.SIZE_X/2, self.SIZE_X/2)
+        y = np.arange(-self.SIZE_Y/2, self.SIZE_Y/2)
         xx, yy = np.meshgrid(x, y)
         return np.sqrt(xx**2 + yy**2)
     
-    def kernel_core(self, x):
-        pass
+    def kernel_core(self, Br ,function="polynomial",alpha= 4):
+        if function == "polynomial":
+            return (4*Br*(1-Br))**alpha
+        elif function == "exponential":
+            return np.exp(alpha - (alpha/(4*Br*(1-Br))))
+
+    """
+    function run_automaton(@world, @kernel, @kernel_FFT, mu, sigma, dt)
+  if size(@world) is small
+    @potential = elementwise_convolution(@kernel, @world)
+  else
+    @world_FFT = FFT_2D(@world)
+    @potential_FFT = elementwise_multiply(@kernel_FFT, @world_FFT)
+    @potential = FFT_shift(real_part(inverse_FFT_2D(@potential_FFT)))
+  end
+  @growth = growth_mapping(@potential, mu, sigma)
+  @new_world = clip(@world + dt * @growth, 0, 1)
+  return @new_world, @growth, @potential
+end
+    """
+
+
+lenia = DiscreteLenia(100,100)
+beta = np.array([0.1, 0.2, 0.3])
+dx = 1
+#polar_radius = lenia.get_polar_radius_matrix() / lenia.get_polar_radius_matrix().max()
+#kernel_core = lenia.kernel_core(polar_radius % 1)
+kernel = lenia.pre_calculate_kernel(beta, dx)
+
+plt.figure(figsize=(6, 6))
+plt.imshow(kernel, cmap="viridis", origin="lower")
+plt.colorbar(label="Intensité normalisée")
+plt.title("Étape 4 : Noyau final normalisé (K)")
+plt.axis("off")
+plt.show()
 
 
     
